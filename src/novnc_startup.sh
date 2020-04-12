a756fe5d8c14428a8d22eeed5f4d28f8251e37bf
aurelien <aurelien@duboc.xyz>
2020-04-12 05:38:12 +0200

    Update novnc log management

--- src/novnc_startup.sh
+++ src/novnc_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NO_VNC_HOME="/root/noVNC"
 source /root/.bashrc
+NO_VNC_HOME="/root/noVNC"
 
 ## resolve_vnc_connection
 VNC_IP=$(hostname -i)
@@ -9,11 +9,4 @@ VNC_IP=$(hostname -i)
 ## start noVNC webclient
 echo -e "\n------------------ start noVNC  ----------------------------"
 echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"
-$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> /root/no_vnc_startup.log &
-
-## log connect options
-echo -e "\n\n------------------ noVNC environment started ------------------"
-echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
-
-echo -e "\n------------------ /root/*.log ------------------"
-tail -f /root/*.log
+$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT
